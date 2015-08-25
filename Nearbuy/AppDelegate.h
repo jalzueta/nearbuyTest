b14ddf20f7c652aea65de26d185b2b7c24dedb7d
Javi Alzueta <jalzuetap@gmail.com>
2015-08-25 23:35:24 +0200

    Initial commit

---
+++ Nearbuy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nearbuy
+//
+//  Created by Javi Alzueta on 25/8/15.
+//  Copyright (c) 2015 JavierAlzueta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nearbuy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nearbuy
+//
+//  Created by Javi Alzueta on 25/8/15.
+//  Copyright (c) 2015 JavierAlzueta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
